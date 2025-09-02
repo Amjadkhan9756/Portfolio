cba06ab043c8fc61e108d7ff8b382b67c98bb4ee
Amjad khan <amjadkhann.tech@gmail.com>
2025-09-02 11:16:35 +0530

    adding new styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import Home from "./Component/Home";
 import Navbar from "./Navbar";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Home />
-      <h1>Hello</h1>
+    
     </BrowserRouter>
   );
 }
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,8 +1,18 @@
+import "./Home.css";
+
 function Home() {
   return (
     <>
-      <div className="w-96 h-64 bg-blue-50 shadow-md rounded-xl flex items-center justify-center">
-        <p className="text-blue-700 font-semibold text-lg">Welcome Home!</p>
+      <div className="container mt-4 w-90 h-90 bg-gradient-to-r from-gray-400 to-gray-500 shadow-md">
+        <div className="row text-center">
+          <div className="col  mt-5 ml-4 w-80 h-70 d-flex align-items-center justify-content-center text-white rounded">
+            <img className="w-40 h-40" src="/image/IMG_20240919_145329_007.jpg" alt="Portfolio" />
+          </div>
+          <div className="col"></div>
+          <div className="col  mt-5 mr-4 w-80 h-70 d-flex align-items-center justify-content-center text-white rounded">
+            Hello
+          </div>
+        </div>
       </div>
     </>
   );
