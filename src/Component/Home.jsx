d34692cf4be1f4d13a426b5d0acbf572dc1cbada
Amjad khan <amjadkhann.tech@gmail.com>
2025-09-02 21:31:32 +0530

    change styling

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -3,10 +3,14 @@ import "./Home.css";
 function Home() {
   return (
     <>
-      <div className="container mt-4 w-90 h-90 bg-gradient-to-r from-gray-400 to-gray-500 shadow-md">
+      <div className="container mx-auto mt-4 w-4/5 h-96 bg-gradient-to-r from-gray-80 to-gray-300  p-6">
         <div className="row text-center">
           <div className="col  mt-5 ml-4 w-80 h-70 d-flex align-items-center justify-content-center text-white rounded">
-            <img className="w-40 h-40" src="/image/IMG_20240919_145329_007.jpg" alt="Portfolio" />
+            <img
+              className="w-40 h-40"
+              src="/image/IMG_20240919_145329_007.jpg"
+              alt="Portfolio"
+            />
           </div>
           <div className="col"></div>
           <div className="col  mt-5 mr-4 w-80 h-70 d-flex align-items-center justify-content-center text-white rounded">
