054521c6c56dd604a881a3bf0299231b89a51b54
Amjad khan <amjadkhann.tech@gmail.com>
2025-09-03 08:38:16 +0530

    new styling

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -3,17 +3,21 @@ import "./Home.css";
 function Home() {
   return (
     <>
-      <div className="container mx-auto mt-4 w-4/5 h-96 bg-gradient-to-r from-gray-80 to-gray-300  p-6">
+      <div className="container mx-auto mt-4 w-4/5 h-120 bg-gradient-to-r from-gray-80 to-gray-300  p-6 shadow hover:shadow-lg transition duration-300">
         <div className="row text-center">
-          <div className="col  mt-5 ml-4 w-80 h-70 d-flex align-items-center justify-content-center text-white rounded">
+          <div className="col p-5  ml-4 w-100 h-90 d-flex align-items-center justify-content-center text-white rounded bg-amber-300">
             <img
-              className="w-40 h-40"
+              className="w-100 h-90"
               src="/image/IMG_20240919_145329_007.jpg"
               alt="Portfolio"
             />
           </div>
-          <div className="col"></div>
-          <div className="col  mt-5 mr-4 w-80 h-70 d-flex align-items-center justify-content-center text-white rounded">
+          <div
+            
+            className="col-2"
+          ></div>
+
+          <div className="col p-5  mr-4 w-100 h-90 d-flex align-items-center justify-content-center text-white rounded bg-amber-900">
             Hello
           </div>
         </div>
